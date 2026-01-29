c0773e44136a68208d2b8105fda5fd3ad9d6c4f6
Kiritaku <69980532+Kiritaku@users.noreply.github.com>
2026-01-29 17:52:49 -0500

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -37,7 +37,7 @@ export const siteConfig = {
   experience: [
     {
       company: "Cummins Inc.",
-      title: "Co-op Engineer - Electronic Hardware Integration,
+      title: "Co-op Engineer - Electronic Hardware Integration",
       dateRange: "June 2025 - Dec 2025",
       bullets: [
         "yuh ma boi",
